e3d579c3a23470bb67e346d3c39545c35f7ff86a
Fima <amifubar@gmail.com>
2018-12-15 23:28:45 +0200

    update gulpfile 2.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,30 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass');
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync');
+
 
 gulp.task('sass', function(){
-	return gulp.src('app/sass/main.sass')
+	return gulp.src('app/sass/**/*.sass')
 	.pipe(sass())
 	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.reload({stream:true}))
 });
+
+//Reload browser
+gulp.task('browser-sync', function(){
+	browserSync({
+		server:{
+			baseDir: 'app'
+		},
+		notify: false
+	});
+});
+
+gulp.task('watch', gulp.series('sass','browser-sync'), function(){
+	gulp.watch('app/sass/*.sass', gulp.series('sass'));
+	gulp.watch('app/**/*.html', browserSync.reload);
+	gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
 
 
