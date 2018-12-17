87558985d12536520a8e157e645d946ef17ac4ff
Fima <amifubar@gmail.com>
2018-12-17 22:32:20 +0200

    gulp 3.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,40 @@
-var gulp = require('gulp'),
-	sass = require('gulp-sass'),
+var gulp        = require('gulp'),
+	sass        = require('gulp-sass'),
 	browserSync = require('browser-sync'),
-	concat = require('gulp-concat'),
-	uglify = require('gulp-uglifyjs');
-
+	concat      = require('gulp-concat'),
+	uglify      = require('gulp-uglifyjs'),
+	cssnano     = require('gulp-cssnano'),
+	rename      = require('gulp-rename'),
+	del         = require('del'),
+	imagemin    = require('gulp-imagemin'),
+	qngquant    = require('imagemin-pngquant'),
+	cache       = require('gulp-cache');
 
 gulp.task('sass', function(){
 	return gulp.src('app/sass/**/*.sass')
 	.pipe(sass())
 	.pipe(gulp.dest('app/css'))
 	.pipe(browserSync.reload({stream:true}))
+});
+
+//transform jquery.js to min.js
+gulp.task('scripts', function(){
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
+		])
+	.pipe(concat('libs.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('app/js'));
+});
+
+//Сompression libs css
+
+gulp.task('css-libs', function(){
+	return gulp.src('app/css/libs.css')
+	.pipe(cssnano())
+	.pipe(rename({suffix:'.min'}))
+	.pipe(gulp.dest('app/css'));
 });
 
 //Reload browser
@@ -21,6 +46,26 @@ gulp.task('browser-sync', function(){
 		notify: false
 	});
 });
+//Image
+gulp.task('clear', function(){
+	return cache.clearAll();
+});
+
+gulp.task('img', function(){
+	return gulp.src('app/img/**/*')
+	.pipe(cache(imagemin({
+		interlaced: true,
+		progressive: true,
+		svgoPlugins:[{removeViewBox: false}],
+		use:[pngquant()]
+	})))
+	.pipe(gulp.dest('dist/img'));
+});
+
+//Clean build
+gulp.task('clean', function(){
+	return del.sync('dist');
+});
 
 gulp.task('watch', function(){
 	gulp.watch('app/sass/*.sass', gulp.series('sass'));
@@ -30,3 +75,21 @@ gulp.task('watch', function(){
 
 gulp.task('default', gulp.parallel('watch','sass','browser-sync'));
 
+gulp.task('build', function(){
+	var buildCss = gulp.src([
+		'app/css/min.css',
+		'app/css/libs.min.css',
+		])
+	.pipe(gulp.dest('dist/css'));
+	var buildFonts = gulp.src('app/fonts/**/*')
+	.pipe(gulp.dest('dist/fonts'));
+
+	var buildJs = gulp.src('app/js/**/*')
+	.pipe(gulp.dest('dist/js'));
+
+	var boildHtml = gulp.src('app/*.html')
+
+});
+
+//Update or create dist
+gulp.task('dist', gulp.series(gulp.parallel('clean','img','sass','scripts'),'build'));
